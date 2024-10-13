8be72def0d1f3314c554415d302cdecb7f51c480
ian-mcr <115840092+ian-mcr@users.noreply.github.com>
2024-10-13 17:09:26 +0100

    Add files via upload

--- RRR.py
+++ RRR.py
@@ -8,15 +8,21 @@ level=1
 trash=["bottle","chips","plastic bag","battery"]
 cats=[]
 actors=[]
+game_over=False
 def draw():
     screen.blit("rbg", (0, 0))
-    for actor in actors:
-        actor.draw()
+    if game_over==False:
+        for actor in actors:
+            actor.draw()
+    else:
+        screen.draw.text("GAME OVER",(340,241),color="black",fontsize=90)
+
 def update():
     pass
 def make_actors():
     global actors 
     images=["paper"]
+    actors.clear()
     for i in range(level):
         number=random.randint(0,3)
         images.append(trash[number])
@@ -29,12 +35,35 @@ def make_actors():
     number_gappes=level+2
     size=WIDTH/number_gappes
     number=1
+    random.shuffle(actors)
     for actor in actors:
         actor.pos=(size*number,50)
         number=number+1
     #animation
     for actor in actors:
         actor.anchor=("center","bottom")
-        dance=animate(actor,duration=5,y=HEIGHT)
+        dance=animate(actor,duration=5,y=HEIGHT,on_finished=gameover)
+        cats.append(dance)
+def stop():
+    for dance in cats:
+        if dance.running:
+            dance.stop()
+
+def gameover():
+    global game_over
+    game_over=True
+
+def on_mouse_down(pos):
+    global level
+    for actor in actors:
+        if actor.collidepoint(pos):
+            if actor.image in "paper":
+                level=level+1
+                stop()
+                make_actors()
+            else:
+                gameover()
+
+
 make_actors()
 pgzrun.go()
